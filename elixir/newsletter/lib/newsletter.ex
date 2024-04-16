e35f65b6beb3f9528f3b2685e7a3d9e7f9677009
Pedro Ferreira <pferreira59@gmail.com>
2024-04-16 13:27:11 +0100

    newsletter

---
+++ elixir/newsletter/lib/newsletter.ex
@@ -0,0 +1,32 @@
+defmodule Newsletter do
+  def read_emails(path) do
+    File.read!(path)
+    |> String.split()
+  end
+
+  def open_log(path) do
+    File.open!(path, [:write])
+  end
+
+  def log_sent_email(pid, email) do
+    IO.puts(pid, email)
+  end
+
+  def close_log(pid) do
+    File.close(pid)
+  end
+
+  def send_newsletter(emails_path, log_path, send_fun) do
+    pid = open_log(log_path)
+
+    emails_path
+    |> read_emails()
+    |> Enum.each(fn email ->
+      if send_fun.(email) == :ok do
+        log_sent_email(pid, email)
+      end
+    end)
+
+    close_log(pid)
+  end
+end
