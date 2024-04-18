98710685e8254d0b4da81dc4e8d2006860c7d52f
Pedro Ferreira <pferreira59@gmail.com>
2024-04-18 17:32:16 +0100

    log-parser

---
+++ elixir/log-parser/lib/log_parser.ex
@@ -0,0 +1,24 @@
+defmodule LogParser do
+  def valid_line?(line) do
+    line =~ ~r/^\[DEBUG|INFO|WARNING|ERROR]/
+  end
+
+  def split_line(line) do
+    separator = ~r/<[~*=-]*>/
+    String.split(line, separator)
+  end
+
+  def remove_artifacts(line) do
+    String.replace(line, ~r/end-of-line(\d+)/i, "")
+  end
+
+  def tag_with_user_name(line) do
+    match = Regex.run(~r/User\s+(\S+)/, line)
+
+    if match do
+      "[USER] #{Enum.at(match, 1)} " <> line
+    else
+      line
+    end
+  end
+end
