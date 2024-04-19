f5fea6da35eb9ae2b5130a0cf35c2a4bee7a32f9
Pedro Ferreira <pferreira59@gmail.com>
2024-04-19 16:52:25 +0100

    scrabble-score iteration 2

--- elixir/scrabble-score/lib/scrabble.ex
+++ elixir/scrabble-score/lib/scrabble.ex
@@ -5,31 +5,33 @@ defmodule Scrabble do
   @spec score(String.t()) :: non_neg_integer
   def score(word) do
     word
-    |> String.graphemes()
-    |> Enum.map(&String.upcase/1)
-    |> Enum.filter(&(&1 not in [" ", "\t", "\n"]))
+    |> String.upcase()
+    |> to_charlist()
     |> Enum.reduce(0, fn letter, acc ->
       case letter do
-        l when l in ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"] ->
+        l when l in ~c"AEIOULNRST" ->
           acc + 1
 
-        l when l in ["D", "G"] ->
+        l when l in ~c"DG" ->
           acc + 2
 
-        l when l in ["B", "C", "M", "P"] ->
+        l when l in ~c"BCMP" ->
           acc + 3
 
-        l when l in ["F", "H", "V", "W", "Y"] ->
+        l when l in ~c"FHVWY" ->
           acc + 4
 
-        l when l in ["K"] ->
+        l when l in ~c"K" ->
           acc + 5
 
-        l when l in ["J", "X"] ->
+        l when l in ~c"JX" ->
           acc + 8
 
-        l when l in ["Q", "Z"] ->
+        l when l in ~c"QZ" ->
           acc + 10
+
+        _ ->
+          0
       end
     end)
   end
