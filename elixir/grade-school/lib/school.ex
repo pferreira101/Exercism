7f1923b682ae8643d50fd02fc6138e1b8fff9a01
Pedro Ferreira <pferreira59@gmail.com>
2024-04-23 15:34:06 +0100

    grade-school

---
+++ elixir/grade-school/lib/school.ex
@@ -0,0 +1,45 @@
+defmodule School do
+  @moduledoc """
+  Simulate students in a school.
+
+  Each student is in a grade.
+  """
+
+  @type school :: any()
+
+  @doc """
+  Create a new, empty school.
+  """
+  @spec new() :: school
+  def new(), do: %{}
+
+  @doc """
+  Add a student to a particular grade in school.
+  """
+  @spec add(school, String.t(), integer) :: {:ok | :error, school}
+  def add(school, name, grade) do
+    case Map.get(school, name) do
+      nil -> {:ok, Map.put(school, name, grade)}
+      _ -> {:error, school}
+    end
+  end
+
+  @doc """
+  Return the names of the students in a particular grade, sorted alphabetically.
+  """
+  @spec grade(school, integer) :: [String.t()]
+  def grade(school, grade) do
+    Enum.filter(school, &(elem(&1, 1) === grade))
+    |> Enum.map(&elem(&1, 0))
+  end
+
+  @doc """
+  Return the names of all the students in the school sorted by grade and name.
+  """
+  @spec roster(school) :: [String.t()]
+  def roster(school) do
+    school
+    |> Enum.sort_by(fn {n, g} -> {g, n} end)
+    |> Enum.map(&elem(&1, 0))
+  end
+end
